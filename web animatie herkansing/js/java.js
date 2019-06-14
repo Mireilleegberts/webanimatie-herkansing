3a1c5efedb9a05422f81dc22b58c5b9b05a91668
Mireilleegberts <33252013+Mireilleegberts@users.noreply.github.com>
2019-06-14 13:17:17 +0200

    Web animatie herkansing (def)

--- web animatie herkansing/js/java.js
+++ web animatie herkansing/js/java.js
@@ -1,19 +1,38 @@
+var btn = document.querySelector(".darkModes");
 var komma = document.getElementById("komma");
 var lightm = document.getElementById("m");
-var lighto = document.getElementById("");
+var lighto = document.getElementById("o");
+var lineColor = document.getElementById("line");
+var tekstColor = document.getElementById("tekst");
+var kommaColor = document.getElementById("komma");
+var newBackground= document.querySelector(".content-container");
+
+var vliegtuig = document.querySelector(".plane");
+
+console.log(lighto)
 
 komma.addEventListener('click', function () {
     komma.classList.toggle('kommaMove');
 
 });
 
-lightm.addEventListener('click', function () {
-    lightm.toggle('Mkleur');
-    lighto.toggle('#oKleur');
+btn.addEventListener('click', function () {
+    btn.classList.toggle('btnLight');
+    lightm.classList.toggle('Mkleur');
+    lighto.classList.toggle('oKleur');
+    tekstColor.classList.toggle('textLight');
+    lineColor.classList.toggle('lineKleur');
+    kommaColor.classList.toggle('kommaKleur');
+    newBackground.classList.toggle('achtergrondDonker');
 });
 
-lighto.addEventListener('click', function () {
-    lighto.toggle('#oKleur');
-});
 
-console.log("Hello world!");
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+        vliegtuig.classList.toggle('planeMove');
+    }
+    if(e.keyCode == 77){
+        lightm.classList.toggle('mKey');
+    }
+}
+
